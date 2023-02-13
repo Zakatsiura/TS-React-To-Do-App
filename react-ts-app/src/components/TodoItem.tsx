854c6990ad0133fd2eff5fba3e1231b4df345ab5
Zakatsiura Ihor <zakatsiura@gmail.com>
2023-02-13 11:40:34 +0200

    pre-final

--- react-ts-app/src/components/TodoItem.tsx
+++ react-ts-app/src/components/TodoItem.tsx
@@ -10,8 +10,20 @@ const TodoItem: React.FC<ITodoItem> = (props) => {
 
     return <div>
         <input type='checkbox' checked={complete} onChange={() => toggleTodo(id)} />
+        <span style={{
+            display: 'inline-block',
+            margin: '0 10px',
+        }}>
         {title}
-        <button onClick={() => removeTodo(id)}>x</button>
+        </span>
+        <button onClick={() => removeTodo(id)}
+                style={{
+                    background: 'transparent',
+                    border: 'none',
+                    outline: 'none',
+                    color: 'red',
+                }}
+        >x</button>
     </div>
 }
 
